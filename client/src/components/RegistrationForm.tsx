3f0701acd8b89da215fc56ecf77e493a9729d7f3
Mike Coon <mac718@gmail.com>
2022-03-11 18:25:25 -0800

    change input value recording from ref based to state based in RegistrationForm

--- client/src/components/RegistrationForm.tsx
+++ client/src/components/RegistrationForm.tsx
@@ -1,20 +1,35 @@
-import { FormEventHandler, useRef } from "react";
+import { ChangeEvent, useState } from "react";
 import styles from "./RegistrationForm.module.css";
 import Button from "./UI/Button";
 
 const RegistrationForm = () => {
-  let nameRef = useRef<HTMLInputElement>(null);
-  let locationRef = useRef<HTMLInputElement>(null);
-  let passwordRef = useRef<HTMLInputElement>(null);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [password, setPassword] = useState("");
+  // let nameRef = useRef<HTMLInputElement>(null);
+  // let locationRef = useRef<HTMLInputElement>(null);
+  // let passwordRef = useRef<HTMLInputElement>(null);
+
+  const setNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const setLocationHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocation(e.target.value);
+  };
+
+  const setPasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
   const registerHandler = (e: any) => {
     e.preventDefault();
     fetch("http://localhost:3001/api/v1/users", {
       method: "POST",
       body: JSON.stringify({
-        name: nameRef.current,
-        location: locationRef.current,
-        password: passwordRef.current,
+        name,
+        location,
+        password,
       }),
       headers: {
         "Content-type": "application/json",
@@ -28,11 +43,11 @@ const RegistrationForm = () => {
     <form className={styles.form} onSubmit={registerHandler}>
       <h2>Sign Up!</h2>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" ref={nameRef} />
+      <input type="text" id="name" onChange={setNameHandler} />
       <label htmlFor="location">Location</label>
-      <input type="text" id="location" ref={locationRef} />
+      <input type="text" id="location" onChange={setLocationHandler} />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" ref={passwordRef} />
+      <input type="password" id="password" onChange={setPasswordHandler} />
       <Button>Submit</Button>
     </form>
   );
